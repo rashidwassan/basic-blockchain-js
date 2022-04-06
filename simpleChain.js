8281baa4dafd7c9ce10bee33557459da501e9f5b
rashidwassaan <rashidwassaan@gmail.com>
2022-04-07 00:53:15 +0500

    added blockchain class

--- simpleChain.js
+++ simpleChain.js
@@ -4,6 +4,16 @@ class Block {
             this.height = 0,
             this.body = data,
             this.time = 0,
-            this.previousblockhash = ""
+            this.previousblockhash = "";
+    }
+}
+
+class BlockChain{
+    constructor(){
+        this.chain = [];
+    }
+
+    addBlock(newBlock){
+        this.chain.push(newBlock);
     }
 }
