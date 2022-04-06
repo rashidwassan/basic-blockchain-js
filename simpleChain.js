ade6b10716136588f6f5bbf364115867c6a6a318
rashidwassaan <rashidwassaan@gmail.com>
2022-04-07 00:29:02 +0500

    added block class

---
+++ simpleChain.js
@@ -0,0 +1,9 @@
+class Block {
+    constructor(data) {
+            this.hash = "",
+            this.height = 0,
+            this.body = data,
+            this.time = 0,
+            this.previousblockhash = ""
+    }
+}
